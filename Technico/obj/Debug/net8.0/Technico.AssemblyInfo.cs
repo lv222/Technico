cd143bf6ed78c686434cf307b6b7f9ca28f64067
Lena <lena.vouimta@gmail.com>
2024-12-31 12:38:48 +0200

    first commit

--- Technico/obj/Debug/net8.0/Technico.AssemblyInfo.cs
+++ Technico/obj/Debug/net8.0/Technico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Technico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+599e5b26aaae2c910ba900f9a7d6bc94d8bb039c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Technico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Technico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
